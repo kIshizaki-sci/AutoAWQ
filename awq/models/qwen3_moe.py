e416be8d24f37ed596e88ac4e1d0d8bb1044a0c2
Kohei ISHIZAKI <ishizaki@superstring.dev>
2025-05-04 08:52:15 +0000

    change qwen3_moe for multiple scale for each expert

--- awq/models/qwen3_moe.py
+++ awq/models/qwen3_moe.py
@@ -2,6 +2,10 @@ import tqdm
 from typing import List, Tuple
 from .base import BaseAWQForCausalLM
 
+from transformers.models.qwen3_moe.modeling_qwen3_moe import Qwen3MoeSparseMoeBlock
+import torch
+from torch import nn
+import types
 
 class Qwen3MoeAWQForCausalLM(BaseAWQForCausalLM):
     layer_type = "Qwen3MoeDecoderLayer"
@@ -13,7 +17,18 @@ class Qwen3MoeAWQForCausalLM(BaseAWQForCausalLM):
 
     @staticmethod
     def get_act_for_scaling(module):
-        return dict(is_scalable=False)
+        scales = []
+        if isinstance(module.mlp, Qwen3MoeSparseMoeBlock):
+            for i in range(module.mlp.experts):
+                scales.append(
+                    dict(
+                        scale_name=f"mlp.experts.{i}.dummy_fn",
+                        scale_layer=module.mlp.experts[i].dummy_fn,
+                        scale_shape=module.mlp.experts[i].gate_proj.in_features
+                    )
+                )
+        
+        return dict(is_scalable=True, scales = scales)
 
     @staticmethod
     def move_embed(model, device: str):
@@ -65,8 +80,17 @@ class Qwen3MoeAWQForCausalLM(BaseAWQForCausalLM):
                 )
             )
 
+            # hange the prev_op to dummy_fn inserted in each expert and apply the scale parameter associated with dummy_fn.
             # linear out
             for i, expert in enumerate(module.mlp.experts):
+                layers.append(
+                    dict(
+                        prev_op=expert.dummy_fn,
+                        layers=[expert.gate_proj, expert.up_proj],
+                        inp=input_feat["mlp"],
+                        module2inspect=module.mlp,
+                    )
+                )
                 layers.append(
                     dict(
                         prev_op=expert.up_proj,
@@ -97,4 +121,72 @@ class Qwen3MoeAWQForCausalLM(BaseAWQForCausalLM):
 
         return layers
 
+    @classmethod
+    def from_pretrained(
+        self,
+        model_path,
+        model_type,
+        torch_dtype = torch.float16,
+        trust_remote_code = True,
+        safetensors = True,
+        device_map = "auto",
+        download_kwargs = None,
+        low_cpu_mem_usage = True,
+        use_cache = False,
+        **model_init_kwargs,
+    ):
+        awq_model = super().from_pretrained(
+            model_path,
+            model_type,
+            torch_dtype,
+            trust_remote_code,
+            safetensors,
+            device_map,
+            download_kwargs,
+            low_cpu_mem_usage,
+            use_cache,
+            **model_init_kwargs,
+        )
+        awq_model._insert_dummy_fn()
+        return awq_model
 
+    def _load_quantized_modules(
+        self, model, quant_config, version, use_exllama, use_exllama_v2, use_ipex=False
+    ):
+        self._insert_dummy_fn()
+        super()._load_quantized_modules(model, quant_config, version, use_exllama, use_exllama_v2, use_ipex)
+
+    @staticmethod
+    def _scale_activations(self, layer):
+        scale_dict = self.get_act_for_scaling(layer)
+        scales = scale_dict.get('scales')
+        if scale_dict["is_scalable"] and scales is None:
+            scales = [scale_dict]
+        if scale_dict["is_scalable"]:
+            for scale in scales:
+                if not isinstance(scale["scale_layer"], ScaledActivation):
+                    param = next(layer.parameters())
+    
+                    # get activation scale
+                    scale_like = torch.ones(
+                        scale["scale_shape"], dtype=param.dtype, device=param.device
+                    )
+    
+                    # scale activation
+                    scaled_act = ScaledActivation(scale["scale_layer"], scale_like)
+                    set_op_by_name(layer, scale["scale_name"], scaled_act)
+
+    def _insert_dummy_fn(self):
+        def forward(self, x):
+            x = self.dummy_fn(x)
+            down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+            return down_proj
+            
+        for layer in self.model.model.layers:
+            if isinstance(layer.mlp, Qwen3MoeSparseMoeBlock):
+                for expert in layer.mlp.experts:
+                    expert.dummy_fn = nn.Identity()
+                    expert.forward = types.MethodType(forward, expert)
+
+
+    
